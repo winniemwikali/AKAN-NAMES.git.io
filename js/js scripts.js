c05dad840e0f3f5bf65a91478f2cea607553aa86
moringaschool-Winnie <mwikaliwinnie303@gmail.com>
2019-11-29 11:29:30 +0300

    added index.html and styles.css

---
+++ js/js scripts.js
@@ -0,0 +1,5 @@
+function getName(){
+    var DayOfMonth=document.getElementsByClassName("Day").value;
+    var month=document.getElementsByClassName("month").value;
+    var year=document.getElementsByClassName("year").value;
+}
