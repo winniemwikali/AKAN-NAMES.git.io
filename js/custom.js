9b34a91c9d3498aed0afa70ec2946d7c4e15305a
moringaschool-Winnie <mwikaliwinnie303@gmail.com>
2019-12-01 12:21:21 +0300

    added checkbox, user can choose gender

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,23 @@
-var getdate = function() {
-    var maleNames = ['Kwasi','Kwandwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']    
-    var femaleNames = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
-    
+
+  
+  var submission = function() {
+    var maleNames = ['Kwasi', 'Kwandwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+    var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+  
     var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-      
+  
     var date = document.getElementById("d").value;
-        var month = document.getElementById("m").value;
-        var year = document.getElementById("yy").value;
-        var male= document.getElementById("male");
-        var female = document.getElementById("female")   
-        var currentDate = new Date(year + "/" + month + "/" + date);
-        var currentYear = currentDate.getDay();
-        if (male.checked) {
-            document.getElementById("ppp").innerHTML = "your Akan name is " + maleNames[currentYear] + "and you were born on " + daysOfTheWeek[currentYear]
-         }
-         else if  (female.checked) {
-            document.getElementById("ppp").innerHTML = "your Akan name is " + femaleNames[currentYear] + "and you were born on " + daysOfTheWeek[currentYear]
-        }
-    
+    var month = document.getElementById("m").value;
+    var year = document.getElementById("yy").value;
+    var male = document.getElementById("male");
+    var female = document.getElementById("female")
+    var currentDate = new Date(year + "/" + month + "/" + date);
+    var currentYear = currentDate.getDay();
+    if (male.checked) {
+      document.getElementById("ppp").innerHTML = "your Akan name is " + maleNames[currentYear] + "and you were born on " + daysOfTheWeek[currentYear]
+    } else if (female.checked) {
+      document.getElementById("ppp").innerHTML = "your Akan name is " + femaleNames[currentYear] + "and you were born on " + daysOfTheWeek[currentYear]
     }
-    
+  
+  }
+  
